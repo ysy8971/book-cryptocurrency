6fac46163b7dbc08cf55ecdeaa76e7a61e31390b
ysy8971 <70571566+ysy8971@users.noreply.github.com>
2021-03-07 14:07:36 +0900

    Update 07_15.py
    1. Line  8 lead to error in previous code. It seems some new coins created after 2018 cause the error. 
    2. Modification, which is not executed in Github, is already applied in wikidocs(https://wikidocs.net/21892)

--- ch07/07_15.py
+++ ch07/07_15.py
@@ -3,24 +3,28 @@ import numpy as np
 
 
 def get_hpr(ticker):
-    df = pybithumb.get_ohlcv(ticker)
-    df = df['2018']
+    try:
+        df = pybithumb.get_ohlcv(ticker)
+        df = df['2018']
+        
+        df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
+        df['range'] = (df['high'] - df['low']) * 0.5
+        df['target'] = df['open'] + df['range'].shift(1)
+        df['bull'] = df['open'] > df['ma5']
+        
+        fee = 0.0032
+        df['ror'] = np.where((df['high'] > df['target']) & df['bull'],
+                             df['close'] / df['target'] - fee,
+                             1)
+        
+        df['hpr'] = df['ror'].cumprod()
+        df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+        return df['hpr'][-2]
+    
+    except:
+        return 1
 
-    df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
-    df['range'] = (df['high'] - df['low']) * 0.5
-    df['target'] = df['open'] + df['range'].shift(1)
-    df['bull'] = df['open'] > df['ma5']
-
-    fee = 0.0032
-    df['ror'] = np.where((df['high'] > df['target']) & df['bull'],
-                          df['close'] / df['target'] - fee,
-                          1)
-
-    df['hpr'] = df['ror'].cumprod()
-    df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
-    return df['hpr'][-2]
-
-
+    
 tickers = pybithumb.get_tickers()
 
 hprs = []
